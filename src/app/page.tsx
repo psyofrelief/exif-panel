74184bf02c3d736bb8bf4222d64ec107171b6159
Faried Idris <107062781+psyofrelief@users.noreply.github.com>
2025-12-04 15:08:41 +0700

    fix: remove unused deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
 "use client";
-import { useEffect } from "react"; // 👈 Import useEffect
 import XmpPanel from "@/features/Analyser/sections/ExifPanel";
 import IllustratorPanel from "@/features/Analyser/sections/XmpPanel";
 import ImagePanel from "@/features/Analyser/sections/ImagePanel";
 
 export default function Home() {
-  // useEffect(() => {
-  //   // 1. Set overflow to hidden when the component mounts
-  //   document.body.style.overflowY = "hidden";
-
-  //   return () => {
-  //     document.body.style.overflowY = "auto";
-  //   };
-  // }, []);
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 relative bg-background h-screen-minus-navbar overflow-y-scroll lg:overflow-y-clip gap-y-[64px] sm:gap-y-xl">
       <ImagePanel />
